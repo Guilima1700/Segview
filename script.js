2b9f2705e4db163afe017621e664511fce4e2bb3
Heitor Fernando <heitorformativo@gmail.com>
2024-12-21 14:45:56 -0300

    commit heitor

--- script.js
+++ script.js
@@ -1,193 +1,85 @@
-(function () {
-    var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
 
-    initHeader();
-    initAnimation();
-    addListeners();
+function lerMais(){
+    document.getElementById('mais-texto').style.display = 'block';
+    document.getElementById('botao-ler-mais').style.display = 'none';
+    document.getElementById('botao-ler-menos').style.display = 'block';
+}
 
-    function initHeader() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        target = { x: width / 2, y: height / 2 };
+function lerMenos(){
+    document.getElementById('mais-texto').style.display = 'none';
+    document.getElementById('botao-ler-menos').style.display = 'none';
+    document.getElementById('botao-ler-mais').style.display = 'block';
+}
 
-        largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = height + 'px';
+document.addEventListener('scroll', function () {
+    const menu = document.getElementById('cabecalho');
+    const itensMenu = document.getElementById('menu-cabecalho');
+    const logo = document.getElementById('logo');
 
-        canvas = document.getElementById('demo-canvas');
-        canvas.width = width;
-        canvas.height = height;
-        ctx = canvas.getContext('2d');
 
-        points = [];
-        for (var x = 0; x < width; x = x + width / 20) {
-            for (var y = 0; y < height; y = y + height / 20) {
-                var px = x + Math.random() * width / 20;
-                var py = y + Math.random() * height / 20;
-                var p = { x: px, originX: px, y: py, originY: py };
-                points.push(p);
-            }
+    if (window.scrollY > 50) { 
+        menu.classList.add('cabecalho-sticky');
+        itensMenu.classList.remove('menu-cabecalho');
+        itensMenu.classList.add('menu-cabecalho-sticky');
+        logo.classList.remove('logo');
+        logo.classList.add('logo-sticky');
+    } else {
+        menu.classList.remove('cabecalho-sticky');
+        itensMenu.classList.add('menu-cabecalho');
+        itensMenu.classList.remove('menu-cabecalho-sticky');
+        logo.classList.add('logo');
+        logo.classList.remove('logo-sticky');
+    }
+});
+
+const elementosScroll= document.querySelectorAll('.hidden');
+const scrollLerMais = document.querySelectorAll('.hidden-ler');
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
         }
+    })
+});
 
-        for (var i = 0; i < points.length; i++) {
-            var closest = [];
-            var p1 = points[i];
-            for (var j = 0; j < points.length; j++) {
-                var p2 = points[j];
-                if (!(p1 == p2)) {
-                    var placed = false;
-                    for (var k = 0; k < 5; k++) {
-                        if (!placed) {
-                            if (closest[k] == undefined) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
+elementosScroll.forEach((elementos) => myObserver.observe(elementos));
+scrollLerMais.forEach((ler) => myObserver.observe(ler));
 
-                    for (var k = 0; k < 5; k++) {
-                        if (!placed) {
-                            if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
-                }
-            }
-            p1.closest = closest;
-        }
+const menuMobile = document.getElementById('menu-mobile');
+const simboloMenuMobile = document.getElementById('simbolo-menu-mobile');
+const simboloFecharMenu = document.getElementById('fechar-menu-mobile');
+const itensMenu = document.querySelectorAll('.item-mobile a');
+const conteudoPag = document.querySelectorAll('.conteudo-principal');
 
-        for (var i in points) {
-            var c = new Circle(points[i], 2 + Math.random() * 2, 'rgba(255,255,255,0.3)');
-            points[i].circle = c;
-        }
+function toggleVisibilidadeConteudo(ocultar) {
+    conteudoPag.forEach(elemento => {
+        elemento.classList.toggle('ocultar', ocultar);
+    });
+}
+
+function abrirMenu() {
+    menuMobile.style.display = 'block';
+    simboloMenuMobile.style.display = 'none';
+    simboloFecharMenu.style.display = 'block';
+    toggleVisibilidadeConteudo(true); 
+    document.querySelectorAll('.item-mobile').forEach(item => item.classList.add('hidden'));
+}
+
+function fecharMenu() {
+    menuMobile.style.display = 'none';
+    simboloFecharMenu.style.display = 'none';
+    simboloMenuMobile.style.display = 'block';
+    toggleVisibilidadeConteudo(false); 
+}
+
+menuMobile.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+        fecharMenu();
     }
-
-    function addListeners() {
-        if (!('ontouchstart' in window)) {
-            window.addEventListener('mousemove', mouseMove);
-        }
-        window.addEventListener('scroll', scrollCheck);
-        window.addEventListener('resize', resize);
-    }
-
-    function mouseMove(e) {
-        var posx = posy = 0;
-        if (e.pageX || e.pageY) {
-            posx = e.pageX;
-            posy = e.pageY;
-        } else if (e.clientX || e.clientY) {
-            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-        }
-        target.x = posx;
-        target.y = posy;
-
-        for (var i in points) {
-            let p = points[i];
-            let dx = p.x - target.x;
-            let dy = p.y - target.y;
-            let dist = Math.sqrt(dx * dx + dy * dy);
-
-            if (dist < 200) {
-                p.x += dx * 0.05;
-                p.y += dy * 0.05;
-            }
-        }
-    }
-
-    function scrollCheck() {
-        if (document.body.scrollTop > height) animateHeader = false;
-        else animateHeader = true;
-    }
-
-    function resize() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        largeHeader.style.height = height + 'px';
-        canvas.width = width;
-        canvas.height = height;
-    }
-
-    function initAnimation() {
-        animate();
-        for (var i in points) {
-            shiftPoint(points[i]);
-        }
-    }
-
-    function animate() {
-        if (animateHeader) {
-            ctx.clearRect(0, 0, width, height);
-            for (var i in points) {
-                var dist = Math.sqrt(getDistance(target, points[i]));
-                if (dist < 200) {
-                    points[i].active = 0.5;
-                    points[i].circle.active = 0.7;
-                } else if (dist < 4000) {
-                    points[i].active = 0.3;
-                    points[i].circle.active = 0.5;
-                } else if (dist < 20000) {
-                    points[i].active = 0.1;
-                    points[i].circle.active = 0.3;
-                } else {
-                    points[i].active = 0;
-                    points[i].circle.active = 0;
-                }
-
-                drawLines(points[i]);
-                points[i].circle.draw();
-            }
-        }
-        requestAnimationFrame(animate);
-    }
-
-    function shiftPoint(p) {
-        TweenLite.to(p, 1 + 1 * Math.random(), {
-            x: p.originX - 50 + Math.random() * 100,
-            y: p.originY - 50 + Math.random() * 100, ease: Circ.easeInOut,
-            onComplete: function () {
-                shiftPoint(p);
-            }
-        });
-    }
-
-    function drawLines(p) {
-        if (!p.active) return;
-        for (var i in p.closest) {
-            ctx.beginPath();
-            ctx.moveTo(p.x, p.y);
-            ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,' + p.active + ')';
-            ctx.stroke();
-        }
-    }
-
-    function Circle(pos, rad, color) {
-        var _this = this;
-
-        (function () {
-            _this.pos = pos || null;
-            _this.radius = rad || null;
-            _this.color = color || null;
-        })();
-
-        this.draw = function () {
-            if (!_this.active) return;
-            ctx.beginPath();
-            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,' + _this.active + ')';
-            ctx.fill();
-        };
-    }
-
-    function getDistance(p1, p2) {
-        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-    }
-
-})();
-
-
+});
 
 document.addEventListener("DOMContentLoaded", () => {
     const statsSection = document.querySelector(".stats-section");
@@ -280,15 +172,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+let count = 1;
+document.getElementById('radio1').checked = true;
 
-document.addEventListener('scroll', function() {
-    const section = document.querySelector('.container-3');
-    const img = section.querySelector('img');
+setInterval(function(){
+    nextImage();
+}, 3000);
 
-    const sectionPosition = section.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight / 1.4;
-
-    if (sectionPosition < screenPosition) {
-        img.classList.add('visible');
+function nextImage(){
+    count++;
+    if (count > 9){
+        count = 1;
     }
-});
+
+    document.getElementById('radio' + count).checked = true;
+
+}
